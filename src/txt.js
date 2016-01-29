34d3c61dbd02bc12ae4eafecfa9cd6ec34b8a393
wyvrw <wyvrw@users.noreply.github.com>
2016-01-29 11:23:10 +0530

    reformat code

--- src/txt.js
+++ src/txt.js
@@ -3,207 +3,207 @@
 
 (function () {
 
-	var defaults = {
-		truncateChars: { append: '...', delimiter: ' ' },
-		truncateItems: { append: '...', delimiter: ' ' }
-	};
+    var defaults = {
+        truncateChars: { append: '...', delimiter: ' ', },
+        truncateItems: { append: '...', delimiter: ' ', },
+    };
 
-	var isString = function (variable) {
-		return typeof variable == 'string' || variable instanceof String;
-	};
+    var isString = function (variable) {
+        return typeof variable == 'string' || variable instanceof String;
+    };
 
-	var isNumber = function (variable) {
-		return (typeof variable == 'number' || variable instanceof Number) && !isNaN(variable);
-	};
+    var isNumber = function (variable) {
+        return (typeof variable == 'number' || variable instanceof Number) && !isNaN(variable);
+    };
 
-	txt = {
+    txt = {
 
-		version: '1.0.0',
+        version: '1.0.0',
 
-		eachMatch: function (text  , pattern   , callback                         ) {
-			// txt.eachMatch('text', 'pattern' , function (match, details) { ... })
-			// txt.eachMatch('text', /pattern/g, function (match, details) { ... })
+        eachMatch: function (text  , pattern   , callback                         ) {
+            // txt.eachMatch('text', 'pattern' , function (match, details) { ... })
+            // txt.eachMatch('text', /pattern/g, function (match, details) { ... })
 
-			if (!isString(text) || !(pattern instanceof RegExp || isString(pattern)) || typeof callback != 'function')
-				throw new TypeError('Invalid parameter');
+            if (!isString(text) || !(pattern instanceof RegExp || isString(pattern)) || typeof callback != 'function')
+                throw new TypeError('Invalid parameter');
 
-			if (isString(pattern))
-				pattern = new RegExp(this.escapeRegExp(pattern), 'g');
-			else if (!pattern.global)
-				pattern = new RegExp(pattern.source, pattern.toString().replace(/^.*\//, 'g'));
+            if (isString(pattern))
+                pattern = new RegExp(this.escapeRegExp(pattern), 'g');
+            else if (!pattern.global)
+                pattern = new RegExp(pattern.source, pattern.toString().replace(/^.*\//, 'g'));
 
-			var matches = text.match(pattern);
-			var matchesIndex = 0;
+            var matches = text.match(pattern);
+            var matchesIndex = 0;
 
-			return text.replace(pattern, function (match) {
-				var result = callback(match, {
-					string: arguments[arguments.length - 1],
-					stringIndex: arguments[arguments.length - 2],
-					array: matches,
-					arrayIndex: matchesIndex++,
-					pattern: pattern
-				});
+            return text.replace(pattern, function (match) {
+                var result = callback(match, {
+                    string: arguments[arguments.length - 1],
+                    stringIndex: arguments[arguments.length - 2],
+                    array: matches,
+                    arrayIndex: matchesIndex++,
+                    pattern: pattern,
+                });
 
-				return isString(result) ? result : match;
-			});
-		},
+                return isString(result) ? result : match;
+            });
+        },
 
-		eachDelimiter: function (text  , delimiter   , callback                         ) {
-			// txt.eachDelimiter('text', 'delimiter' , function (match, details) { ... })
-			// txt.eachDelimiter('text', /delimiter/g, function (match, details) { ... })
+        eachDelimiter: function (text  , delimiter   , callback                         ) {
+            // txt.eachDelimiter('text', 'delimiter' , function (match, details) { ... })
+            // txt.eachDelimiter('text', /delimiter/g, function (match, details) { ... })
 
-			if (!isString(text) || !(delimiter instanceof RegExp || isString(delimiter)) || typeof callback != 'function')
-				throw new TypeError('Invalid parameter');
+            if (!isString(text) || !(delimiter instanceof RegExp || isString(delimiter)) || typeof callback != 'function')
+                throw new TypeError('Invalid parameter');
 
-			var joins = [];
-			var delimiters = text.split(delimiter);
-			var delimitersIndex = 0;
-			var newText = '';
+            var joins = [];
+            var delimiters = text.split(delimiter);
+            var delimitersIndex = 0;
+            var newText = '';
 
-			if (delimiter instanceof RegExp) {
-				if (!delimiter.global)
-					delimiter = new RegExp(delimiter.source, delimiter.toString().replace(/.*\//, 'g'));
+            if (delimiter instanceof RegExp) {
+                if (!delimiter.global)
+                    delimiter = new RegExp(delimiter.source, delimiter.toString().replace(/.*\//, 'g'));
 
-				joins = text.match(delimiter) || joins;
-			}
+                joins = text.match(delimiter) || joins;
+            }
 
-			while (delimitersIndex < delimiters.length) {
-				var result = callback(delimiters[delimitersIndex], {
-					string: text,
-					stringIndex: newText.length,
-					array: delimiters,
-					arrayIndex: delimitersIndex++,
-					delimiter: delimiter
-				});
+            while (delimitersIndex < delimiters.length) {
+                var result = callback(delimiters[delimitersIndex], {
+                    string: text,
+                    stringIndex: newText.length,
+                    array: delimiters,
+                    arrayIndex: delimitersIndex++,
+                    delimiter: delimiter,
+                });
 
-				newText += (isString(result) ? result : delimiters[delimitersIndex - 1]) + (delimitersIndex < delimiters.length ? joins.shift() || delimiter : '');
-			}
+                newText += (isString(result) ? result : delimiters[delimitersIndex - 1]) + (delimitersIndex < delimiters.length ? joins.shift() || delimiter : '');
+            }
 
-			return newText;
-		},
+            return newText;
+        },
 
-		truncateChars: function (text  , number, options                          ) {
-			// txt.truncateChars('text', 0     , { append: '...', delimiter: ' ' })
-			// txt.truncateChars('text', 0                                        )
+        truncateChars: function (text  , number, options                          ) {
+            // txt.truncateChars('text', 0     , { append: '...', delimiter: ' ' })
+            // txt.truncateChars('text', 0                                        )
 
-			if (!isString(text) || !isNumber(number))
-				throw new TypeError('Invalid parameter');
+            if (!isString(text) || !isNumber(number))
+                throw new TypeError('Invalid parameter');
 
-			if (number < 0)
-				throw new RangeError('Invalid number');
+            if (number < 0)
+                throw new RangeError('Invalid number');
 
-			if (number >= text.length)
-				return text;
+            if (number >= text.length)
+                return text;
 
-			options = options || {};
+            options = options || {};
 
-			var append = (isString(options.append) ? options.append : defaults.truncateChars.append).slice(0, number);
+            var append = (isString(options.append) ? options.append : defaults.truncateChars.append).slice(0, number);
 
-			text = text.slice(0, number - append.length);
+            text = text.slice(0, number - append.length);
 
-			return text.slice(0, text.lastIndexOf(isString(options.delimiter) ? options.delimiter : defaults.truncateChars.delimiter) + 1 || text.length) + append;
-		},
+            return text.slice(0, text.lastIndexOf(isString(options.delimiter) ? options.delimiter : defaults.truncateChars.delimiter) + 1 || text.length) + append;
+        },
 
-		truncateItems: function (text  , number, options                          ) {
-			// txt.truncateItems('text', 0     , { append: '...', delimiter: ' ' })
-			// txt.truncateItems('text', 0                                        )
+        truncateItems: function (text  , number, options                          ) {
+            // txt.truncateItems('text', 0     , { append: '...', delimiter: ' ' })
+            // txt.truncateItems('text', 0                                        )
 
-			if (!isString(text) || !isNumber(number))
-				throw new TypeError('Invalid parameter');
+            if (!isString(text) || !isNumber(number))
+                throw new TypeError('Invalid parameter');
 
-			if (number < 0)
-				throw new RangeError('Invalid number');
+            if (number < 0)
+                throw new RangeError('Invalid number');
 
-			options = options || {};
+            options = options || {};
 
-			var delimiter = isString(options.delimiter) ? options.delimiter : defaults.truncateItems.delimiter;
+            var delimiter = isString(options.delimiter) ? options.delimiter : defaults.truncateItems.delimiter;
 
-			text = text.split(delimiter);
+            text = text.split(delimiter);
 
-			return (number < text.length ? text.slice(0, number).concat(isString(options.append) ? options.append : defaults.truncateItems.append) : text).join(delimiter);
-		},
+            return (number < text.length ? text.slice(0, number).concat(isString(options.append) ? options.append : defaults.truncateItems.append) : text).join(delimiter);
+        },
 
-		escapeHTML: function (text  ) {
-			// txt.escapeHTML('text')
+        escapeHTML: function (text  ) {
+            // txt.escapeHTML('text')
 
-			if (isString(text))
-				return text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+            if (isString(text))
+                return text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 
-			throw new TypeError('Invalid parameter');
-		},
+            throw new TypeError('Invalid parameter');
+        },
 
-		unescapeHTML: function (text  ) {
-			// txt.unescapeHTML('text')
+        unescapeHTML: function (text  ) {
+            // txt.unescapeHTML('text')
 
-			if (isString(text))
-				return text.replace(/&lt;/gi, '<').replace(/&gt;/gi, '>').replace(/&amp;/gi, '&');
+            if (isString(text))
+                return text.replace(/&lt;/gi, '<').replace(/&gt;/gi, '>').replace(/&amp;/gi, '&');
 
-			throw new TypeError('Invalid parameter');
-		},
+            throw new TypeError('Invalid parameter');
+        },
 
-		escapeRegExp: function (text  ) {
-			// txt.escapeRegExp('text')
+        escapeRegExp: function (text  ) {
+            // txt.escapeRegExp('text')
 
-			if (isString(text))
-				return text.replace(/[\^\$\(\)\[\]\{\}\*\+\?\|\.\-\/\\]/g, '\\$&');
+            if (isString(text))
+                return text.replace(/[\^\$\(\)\[\]\{\}\*\+\?\|\.\-\/\\]/g, '\\$&');
 
-			throw new TypeError('Invalid parameter');
-		},
+            throw new TypeError('Invalid parameter');
+        },
 
-		unescapeRegExp: function (text  ) {
-			// txt.unescapeRegExp('text')
+        unescapeRegExp: function (text  ) {
+            // txt.unescapeRegExp('text')
 
-			if (isString(text))
-				return text.replace(/\\([\^\$\(\)\[\]\{\}\*\+\?\|\.\-\/\\])/g, '$1');
+            if (isString(text))
+                return text.replace(/\\([\^\$\(\)\[\]\{\}\*\+\?\|\.\-\/\\])/g, '$1');
 
-			throw new TypeError('Invalid parameter');
-		},
+            throw new TypeError('Invalid parameter');
+        },
 
-		pluralize: function (rule, rules                       ) {
-			// txt.pluralize(0   , { singular: '', plural: '' })
+        pluralize: function (rule, rules                       ) {
+            // txt.pluralize(0   , { singular: '', plural: '' })
 
-			if (!isNumber(rule))
-				throw new TypeError('Invalid parameter');
+            if (!isNumber(rule))
+                throw new TypeError('Invalid parameter');
 
-			rule = rule == 1 ? 'singular' : 'plural';
+            rule = rule == 1 ? 'singular' : 'plural';
 
-			return rules && isString(rules[rule]) ? rules[rule] : '';
-		},
+            return rules && isString(rules[rule]) ? rules[rule] : '';
+        },
 
-		capitalize: function (text  ) {
-			// txt.capitalize('text')
+        capitalize: function (text  ) {
+            // txt.capitalize('text')
 
-			if (isString(text))
-				return (text[0] || '').toUpperCase() + text.slice(1);
+            if (isString(text))
+                return (text[0] || '').toUpperCase() + text.slice(1);
 
-			throw new TypeError('Invalid parameter');
-		},
+            throw new TypeError('Invalid parameter');
+        },
 
-		defaults: function (newDefaults) {
-			// txt.defaults({ ... }    )
-			// txt.defaults(           )
+        defaults: function (newDefaults) {
+            // txt.defaults({ ... }    )
+            // txt.defaults(           )
 
-			if (typeof newDefaults == 'object') {
-				for (var funcName in newDefaults) {
-					var newFuncDefaults = newDefaults[funcName];
+            if (typeof newDefaults == 'object') {
+                for (var funcName in newDefaults) {
+                    var newFuncDefaults = newDefaults[funcName];
 
-					if (defaults.hasOwnProperty(funcName) && typeof newFuncDefaults == 'object') {
-						var funcDefaults = defaults[funcName];
+                    if (defaults.hasOwnProperty(funcName) && typeof newFuncDefaults == 'object') {
+                        var funcDefaults = defaults[funcName];
 
-						for (var defaultName in newFuncDefaults) {
-							if (funcDefaults.hasOwnProperty(defaultName) && isString(newFuncDefaults[defaultName]))
-								funcDefaults[defaultName] = newFuncDefaults[defaultName];
-						}
-					}
-				}
-			}
+                        for (var defaultName in newFuncDefaults) {
+                            if (funcDefaults.hasOwnProperty(defaultName) && isString(newFuncDefaults[defaultName]))
+                                funcDefaults[defaultName] = newFuncDefaults[defaultName];
+                        }
+                    }
+                }
+            }
 
-			return defaults;
-		}
+            return defaults;
+        },
 
-	};
+    };
 
-	if (typeof module != 'undefined' && module.exports)
-		module.exports = txt;
+    if (typeof module != 'undefined' && module.exports)
+        module.exports = txt;
 
 })();
